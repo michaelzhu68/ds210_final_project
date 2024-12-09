213a8a9ce255b76027f5b9bba7fe11285f257a14
Michael <zhenyun6@bu.edu>
2024-12-09 00:21:33 -0500

    finished similarity function

--- code/src/main.rs
+++ code/src/main.rs
@@ -81,3 +81,56 @@ fn calculate_similarity(m1: &Member, m2: &Member) -> f32 {
     let weight_workout_frequency = 0.2;
     let weight_experience_level = 0.2;
     let weight_bmi = 0.02;
+
+    let mut score = 0.0;
+
+    let age_diff = (m1.age as f32 - m2.age as f32).abs();
+    score += weight_age * (1.0 / (1.0 + age_diff / 10.0));
+
+    if m1.gender == m2.gender {
+        score += weight_gender;
+    }
+
+    let weight_diff = (m1.weight - m2.weight).abs();
+    score += weight_weight * (1.0 / (1.0 + weight_diff / 10.0));
+
+    let height_diff = (m1.height - m2.height).abs();
+    score += weight_height * (1.0 / (1.0 + height_diff));
+
+    let max_bpm_diff = (m1.max_bpm as i32 - m2.max_bpm as i32).abs() as f32;
+    score += weight_max_bpm * (1.0 / (1.0 + max_bpm_diff));
+
+    let avg_bpm_diff = (m1.avg_bpm as i32 - m2.avg_bpm as i32).abs() as f32;
+    score += weight_avg_bpm * (1.0 / (1.0 + avg_bpm_diff));
+
+    let resting_bpm_diff = (m1.resting_bpm as i32 - m2.resting_bpm as i32).abs() as f32;
+    score += weight_resting_bpm * (1.0 / (1.0 + resting_bpm_diff));
+
+    let duration_diff = (m1.session_duration - m2.session_duration).abs();
+    score += weight_session_duration * (1.0 / (1.0 + duration_diff));
+
+    let calories_diff = (m1.calories_burned - m2.calories_burned).abs();
+    score += weight_calories_burned * (1.0 / (1.0 + calories_diff / 1000.0));
+
+    if m1.workout_type == m2.workout_type {
+        score += weight_workout_type;
+    }
+
+    let fat_diff = (m1.fat_percentage - m2.fat_percentage).abs();
+    score += weight_fat_percentage * (1.0 / (1.0 + fat_diff));
+
+    let water_diff = (m1.water_intake - m2.water_intake).abs();
+    score += weight_water_intake * (1.0 / (1.0 + water_diff));
+
+    let frequency_diff = (m1.workout_frequency as i32 - m2.workout_frequency as i32).abs() as f32;
+    score += weight_workout_frequency * (1.0 / (1.0 + frequency_diff));
+
+    if m1.experience_level == m2.experience_level {
+        score += weight_experience_level;
+    }
+
+    let bmi_diff = (m1.bmi - m2.bmi).abs();
+    score += weight_bmi * (1.0 / (1.0 + bmi_diff));
+
+    score
+}
