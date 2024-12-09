cf42dad7fd1edea33e7db68d901030c0bf3e8faa
Michael <zhenyun6@bu.edu>
2024-12-08 19:04:22 -0500

    added all files

---
+++ code/src/main.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug, Clone)]
+struct Member {
+    id: usize,
+    age: u32,
+    gender: String,
+    weight: f32,
+    height: f32,
+    max_bpm: u32,
+    avg_bpm: u32,
+    resting_bpm: u32,
+    session_duration: f32,
+    calories_burned: f32,
+    workout_type: String,
+    fat_percentage: f32,
+    water_intake: f32,
+    workout_frequency: u32,
+    experience_level: u32,
+    bmi: f32,
+    connections: HashSet<usize>, 
+}
+
+impl Member {
+    fn new(id: usize, fields: Vec<&str>) -> Self {
+        Self {
+            id,
+            age: fields[0].parse().unwrap(),
+            gender: fields[1].to_string(),
+            weight: fields[2].parse().unwrap(),
+            height: fields[3].parse().unwrap(),
+            max_bpm: fields[4].parse().unwrap(),
+            avg_bpm: fields[5].parse().unwrap(),
+            resting_bpm: fields[6].parse().unwrap(),
+            session_duration: fields[7].parse().unwrap(),
+            calories_burned: fields[8].parse().unwrap(),
+            workout_type: fields[9].to_string(),
+            fat_percentage: fields[10].parse().unwrap(),
+            water_intake: fields[11].parse().unwrap(),
+            workout_frequency: fields[12].parse().unwrap(),
+            experience_level: fields[13].parse().unwrap(),
+            bmi: fields[14].parse().unwrap(),
+            connections: HashSet::new(),
+        }
+    }
+}
+
+fn parse_csv(file_path: &str) -> Vec<Member> {
+    let file = File::open(file_path).expect("Unable to open file");
+    let reader = BufReader::new(file);
+    let mut members = Vec::new();
+
+    for (idx, line) in reader.lines().enumerate() {
+        let line = line.expect("Unable to read line");
+        if idx == 0 {
+            continue;
+        }
+
+        let fields: Vec<&str> = line.split(',').collect();
+        members.push(Member::new(idx - 1, fields));
+    }
+
+    members
+}
